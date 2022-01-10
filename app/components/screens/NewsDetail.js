50c204602d70b8c32ccc1fe31fe0c5b3abc6ea9e
Alexey Mezentsev <alem_91@mail.ru>
2022-01-10 14:17:32 +0300

    news upd

--- app/components/screens/NewsDetail.js
+++ app/components/screens/NewsDetail.js
@@ -18,11 +18,13 @@ import HorizotalList from '../lists/HorizotalList';
 import Close from '../common/Close';
 import { useNavigation } from '@react-navigation/native';
 import ActivityIndicator from '../common/ActivityIndicator';
+import { SliderBox } from "react-native-image-slider-box";
 
 const { width, height } = Dimensions.get('window');
 
 const NewsDetail = ({ route }) => {
   const [news, setNews] = useState({});
+  const [galleries, setGalleries] = useState([]);
   const [relatedNews, setRelatedNews] = useState([]);
   const { id: postId } = route.params.item;
   const [loading, setLoading] = useState(false);
@@ -32,7 +34,15 @@ const NewsDetail = ({ route }) => {
   const fetchPost = async id => {
     setLoading(true);
     const result = await newsApi.getSingle(id);
+    if(result.gallery && result.gallery.list && result.gallery.list.length) {
+      let galleriesLocal = []
+      for await (let galleryItem of result.gallery.list) {
+        galleriesLocal.push(galleryItem.url);
+      }
+      setGalleries(galleriesLocal)
+    }
     setNews(result);
+    setLoading(false);
   };
 
   // const fetchRelatedPosts = async category => {
@@ -74,11 +84,20 @@ const NewsDetail = ({ route }) => {
           <Text style={styles.title}>{title}</Text>
           <RenderHtml style={styles.content} source={source} />
         </View>
+        <SliderBox
+            images={galleries}
+            sliderBoxHeight={200}
+            onCurrentImagePressed={index => console.warn(`image ${galleries[index]} pressed`)}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            paginationBoxVerticalPadding={20}
+            autoplay
+            circleLoop
+        />
         <View style={styles.relatedPostContainer}>
           <HorizotalList data={relatedNews} title='Related Posts' />
         </View>
       </ScrollView>
-      <Close onPress={() => navigation.popToTop()} />
     </>
   );
 };
